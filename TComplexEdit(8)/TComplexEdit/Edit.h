77e21ac4ec0e6e89055a23788f84553d04786465
Артем <tema.lupandin@mail.ru>
2018-12-11 16:18:05 +0600

    add TComplexEdit

---
+++ TComplexEdit(8)/TComplexEdit/Edit.h
@@ -0,0 +1,23 @@
+#include <string>
+#include <regex>
+using namespace std;
+
+class TComplexEditor {
+public:
+	TComplexEditor();
+	bool isZero();
+	string changeSign();
+	string addDigit(int digit);
+	string addZero();
+	string backspace();
+	string clear();
+	string addSplit();
+	void setNumber(string &num);
+	string getNumber() const;
+	static const string ZERO;
+	static const string SIGN;
+private:
+	string number;
+	bool isRealPartEditing;
+};
+
